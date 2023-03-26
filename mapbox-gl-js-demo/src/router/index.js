f60d055dbb0988493842528e9eab228139a1a298
xiaogeng <359315634@qq.com>
2023-03-26 16:46:21 +0800

    文件位置修改

--- mapbox-gl-js-demo/src/router/index.js
+++ mapbox-gl-js-demo/src/router/index.js
@@ -37,7 +37,9 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/demo/pdfPreview.vue"),
+      import(
+        /* webpackChunkName: "about" */ "../views/demo/js/预览pdf文件/pdfPreview.vue"
+      ),
   },
   {
     path: "/encodeGeojson",
---
+++ mapbox-gl-js-demo/src/views/demo/mapbox_demo/mapbox绘制点图层并进行添加、删除、更新、显隐等操作/mapboxViewPoint.vue
@@ -0,0 +1,175 @@
+<template>
+  <div>
+    <div class="home" id="container">
+      <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    </div>
+    <div id="coordinate_div">
+      <p>经度: <span id="longitude"></span></p>
+      <p>纬度: <span id="latitude"></span></p>
+    </div>
+    <div id="overviewMap" class="overviewMap"></div>
+    <div class="select-box">
+      网格：
+      <el-select
+        v-model="gridValue"
+        @change="(e) => changeGeoJson(e, 'grid')"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in gridOptions"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+    </div>
+    <div class="select-box2">
+      点位数据：
+      <el-select
+        v-model="pointValue"
+        @change="(e) => changeGeoJson(e, 'point')"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in pointOptions"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+const mapboxgl = require("mapbox-gl");
+import interestingPoint from "@/assets/mapData/china.geojson";
+import drawMap from "@/assets/js/drawMapFigure";
+import drawMiniMap from "@/assets/js/drawMiniMap";
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      rotateFlag: false,
+      initFlag: true,
+      startTime: null,
+      gridValue: null,
+      pointValue: null,
+      gridOptions: [
+        {
+          value: "streetGrid",
+          label: "街道网格",
+        },
+        {
+          value: "communityGrid",
+          label: "社区网格",
+        },
+        {
+          value: "county",
+          label: "县区",
+        },
+        {
+          value: "unitGrid",
+          label: "单元网格",
+        },
+        {
+          value: "partArea",
+          label: "地片与片区",
+        },
+        {
+          value: "cityEnvironment",
+          label: "市容环境卫生责任网格",
+        },
+      ],
+      pointOptions: [
+        {
+          value: "interestPoint",
+          label: "兴趣点",
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.map = drawMap("container");
+    // drawMiniMap("overviewMap", this.map);
+
+    this.map.on("mousemove", function (e) {
+      document.getElementById("longitude").innerHTML = e.lngLat.lng.toFixed(5);
+      document.getElementById("latitude").innerHTML = e.lngLat.lat.toFixed(5);
+    });
+
+    this.map.on("load", () => {
+      this.map.addSource("interestingPoint", {
+        type: "geojson",
+        data: interestingPoint,
+      });
+      this.map.addLayer({
+        type: "fill",
+        source: "interestingPoint",
+        maxzoom: 14,
+        id: "10",
+        paint: { "fill-color": "#60D330", "fill-outline-color": "#000000" },
+        // layout: {
+        //   "fill-sort-key": 100000,
+        // },
+      });
+      // this.map.moveLayer("tdtvec", "10");
+    });
+
+    // this.map.on("draw.create", updateArea);
+    // this.map.on("draw.delete", updateArea);
+    // this.map.on("draw.update", updateArea);
+  },
+  methods: {},
+};
+</script>
+<style lang="less" scoped>
+#container {
+  width: 100%;
+  height: 800px;
+}
+
+#overviewMap {
+  width: 200px;
+  height: 200px;
+  position: absolute;
+  right: 20px;
+  bottom: 50px;
+  border: 2px solid gray;
+}
+.mapboxgl-ctrl-attrib {
+  display: none;
+}
+
+#coordinate_div {
+  position: absolute;
+  bottom: 80px;
+  left: 20px;
+  color: white;
+  z-index: 100;
+}
+
+#coordinate_div > p {
+  margin: 2px;
+}
+
+.select-box {
+  position: absolute;
+  top: 100px;
+  left: 20px;
+  z-index: 100;
+  color: white;
+}
+
+.select-box2 {
+  position: absolute;
+  top: 100px;
+  left: 300px;
+  z-index: 100;
+  color: white;
+}
+</style>
