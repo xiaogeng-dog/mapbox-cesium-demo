1aaf5a627a007d286c2d8c18b4d8747fd6d115b8
gengningsheng <359315634@qq.com>
2023-05-06 14:21:53 +0800

    拖拽demo

--- mapbox-gl-js-demo/src/router/index.js
+++ mapbox-gl-js-demo/src/router/index.js
@@ -53,6 +53,15 @@ const routes = [
       ),
   },
   {
+    path: "/vuedraggable",
+    name: "vuedraggable",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/demo/vuedraggable"),
+  },
+  {
     path: "/rsa",
     name: "rsa",
     // route level code-splitting
